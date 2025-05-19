d0c2a86e0da621d9f3010422005507bd8a5a6f5c
sukhmani <sukhmani@bellevuecollege.edu>
2025-05-19 21:01:22 +0000

    subheading

--- CampusBlog/app/(tabs)/about.tsx
+++ CampusBlog/app/(tabs)/about.tsx
@@ -9,6 +9,8 @@ return (
 <View style={styles.container}>
 <Text style={styles.heading}> About the app</Text>
 This project has been implemented and designed by using the MERN stack including React Native framework, Express, Node, MongoDB. The main purpose of this mobile app is to provide a social platform where users can interact with other students through features offered in the application. The application will include authentication, post creation, and real-time interactions. The target users are students who are currently studying or have previously studied at City University of Seattle. This project demonstrates how we apply and practice the knowledge gained in this course to build a mobile application.
+ <Text style={styles.subheading}>Meet the Team</Text>
+
 <Image source={require('../(tabs)/user.png')} style={styles.image}>
 
 </Image>
@@ -45,5 +47,12 @@ fontSize: 26,
 fontWeight: 'bold',
 marginBottom: 20,
 textAlign: 'center'
+  },
+   subheading: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 30,
+    marginBottom: 10,
+    textAlign: 'center',
   },
 });
