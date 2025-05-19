43ad336c31b1905aba73cc35126c15f306df1bf1
sukhmani <sukhmani@bellevuecollege.edu>
2025-05-19 19:39:23 +0000

    About Images

--- CampusBlog/app/(tabs)/about.tsx
+++ CampusBlog/app/(tabs)/about.tsx
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native';
+
 
 export default function AboutScreen() {
 return (
 <View style={styles.container}>
 <Text> About the app</Text>
-<p>This project has been implemented and designed by using the MERN stack including React Native framework, Express, Node, MongoDB. The main purpose of this mobile app is to provide a social platform where users can interact with other students through features offered in the application. The application will include authentication, post creation, and real-time interactions. The target users are students who are currently studying or have previously studied at City University of Seattle. This project demonstrates how we apply and practice the knowledge gained in this course to build a mobile application.</p>
-<Image source= >
+This project has been implemented and designed by using the MERN stack including React Native framework, Express, Node, MongoDB. The main purpose of this mobile app is to provide a social platform where users can interact with other students through features offered in the application. The application will include authentication, post creation, and real-time interactions. The target users are students who are currently studying or have previously studied at City University of Seattle. This project demonstrates how we apply and practice the knowledge gained in this course to build a mobile application.
+<Image source={require('../(tabs)/user.png')} style={styles.image}>
+
+</Image>
+<Image source={require('../(tabs)/Sukhmani.png')} style={styles.image}>
+
+</Image>
+<Image source={require('../(tabs)/Yen.png')} style={styles.image}>
 
 </Image>
 </View>
@@ -19,4 +27,9 @@ container: {
 	alignItems: 'center',
 	justifyContent: 'center',
 },
+ image: {
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
 });
